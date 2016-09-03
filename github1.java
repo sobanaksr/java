d625b65c2157cf8c7f8f26f62ae141ffef1bf91e
sobanaksr <meeragayathiri@gmail.com>
2016-09-03 07:03:23 +0530

    Add files via upload

---
+++ github1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class positive
+{
+	public static void main(String[] arg)
+	{
+		int n;
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter a number:");
+		n=in.nextInt();
+		if(n>0)
+		System.out.print("The no is positive");
+		else if(n=0)
+		System.out.print("The number is equal to zero");
+		else
+		System.out.print("The number is negative");
+
+	}
+}
